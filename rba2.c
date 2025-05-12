26e86f1880267f2e5a8881658d8c5cd0cbc5bd99
thenoobgrammer <thenoobgrammer@gmail.com>
2025-05-11 23:09:14 -0400

    update

--- rba2.c
+++ rba2.c
@@ -10,23 +10,33 @@
 #define MODE1 0X00
 #define PRESCALE 0XFE
 
-void setPWN(int fd, int channel, int on, int off) {
+void setPWM(int fd, int channel, int on, int off)
+{
 	int reg = 0x06 + 4 * channel;
-	i2c_smbus_write_word_data(fd, reg, on & 0xFF | ((on >> 8) << 8));
-	i2c_smbus_write_word_data(fd, reg + 2, off & 0xFF | ((off >> 8) << 8));
+	i2c_smbus_write_byte_data(fd, reg, on & 0xFF);
+	i2c_smbus_write_byte_data(fd, reg + 1, (on >> 8) & 0x0F);
+	i2c_smbus_write_byte_data(fd, reg + 2, off & 0xFF);
+	i2c_smbus_write_byte_data(fd, reg + 3, (off >> 8) & 0x0F);
 }
 
-int main() {
+int main()
+{
 	int fd = open("/dev/i2c-1", O_RDWR);
-	if (fd < 0) { perror("open"); exit(1); }
+	if (fd < 0)
+	{
+		perror("open");
+		exit(1);
+	}
 
-	if (ioctl(fd, I2C_SLAVE, PCA9685_ADDR) < 0) {
-		perror("ioctl"); exit(1);
+	if (ioctl(fd, I2C_SLAVE, PCA9685_ADDR) < 0)
+	{
+		perror("ioctl");
+		exit(1);
 	}
 
 	i2c_smbus_write_byte_data(fd, MODE1, 0x00);
 
-	setPWN(fd, 4, 0, 375);
+	setPWM(fd, 4, 0, 375);
 
 	close(fd);
 	return 0;
