bfa6dd3b9fd54e3cd0b50aec0a8792f9fd370001
thenoobgrammer <thenoobgrammer@gmail.com>
2025-05-12 04:05:25 +0100

    updated c code

---
+++ rba2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <i2c/smbus.h>
+
+#define PCA9685_ADDR 0X40
+#define MODE1 0X00
+#define PRESCALE 0XFE
+
+void setPWN(int fd, int channel, int on, int off) {
+	int reg = 0x06 + 4 * channel;
+	i2c_smbus_write_word_data(fd, reg, on & 0xFF | ((on >> 8) << 8));
+	i2c_smbus_write_word_data(fd, reg + 2, off & 0xFF | ((off >> 8) << 8));
+}
+
+int main() {
+	int fd = open("/dev/i2c-1", O_RDWR);
+	if (fd < 0) { perror("open"); exit(1); }
+
+	if (ioctl(fd, I2C_SLAVE, PCA9685_ADDR) < 0) {
+		perror("ioctl"); exit(1);
+	}
+
+	i2c_smbus_write_byte_data(fd, MODE1, 0x00);
+
+	setPWN(fd, 4, 0, 375);
+
+	close(fd);
+	return 0;
+}
